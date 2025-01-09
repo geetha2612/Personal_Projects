c5788564e823addf3441203248f79551a89138e0
geetha2612 <93509212+geetha2612@users.noreply.github.com>
2025-01-09 20:55:03 +0530

    Add files via upload

---
+++ CourseRegistrationSystem/CourseRegistrySystem/CourseRegistrySystem/src/main/java/com/example/CourseRegistrySystem/CourseRegistrySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.CourseRegistrySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegistrySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseRegistrySystemApplication.class, args);
+	}
+
+}
---
+++ CourseRegistrationSystem/CourseRegistrySystem/CourseRegistrySystem/src/main/java/com/example/CourseRegistrySystem/repository/CourseRepo.java
@@ -0,0 +1,9 @@
+package com.example.CourseRegistrySystem.repository;
+
+import com.example.CourseRegistrySystem.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepo extends JpaRepository<Course,String> {
+}
